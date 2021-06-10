5d72ead5516dffff262c7fe260ae67905ac133b0
vietsaclo <vietsaclo@gmail.com>
2021-06-10 17:04:32 +0700

    FE-ReactJS-Base 01

---
+++ src/common/utils/keys/NotificationKeys.js
@@ -0,0 +1,10 @@
+const NotificationKeys = {
+  INFO: 'INFO',
+  SUCCESS: 'SUCCESS',
+  ERROR: 'ERROR',
+  WARM: 'WARM',
+  WARNING: 'WARNING',
+  LOADING: 'LOADING',
+}
+
+export default NotificationKeys;
---
+++ src/common/utils/keys/index.js
@@ -0,0 +1,9 @@
+import LocalStorageKeys from "./LocalStorageKeys";
+import MessageKeys from './MessageKeys';
+import NotificationKeys from './NotificationKeys';
+
+export {
+  LocalStorageKeys,
+  MessageKeys,
+  NotificationKeys,
+}
